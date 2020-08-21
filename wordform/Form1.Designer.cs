9d6eff1396e623fa1c3b8e92532fad2970018241
kevinkentish <kentishiyasawmy@gmail.com>
2020-08-21 15:43:38 +0400

    First commit in the repo

---
+++ wordform/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace wordform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputWord = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.play = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputWord
+            // 
+            this.InputWord.AutoSize = true;
+            this.InputWord.Location = new System.Drawing.Point(79, 53);
+            this.InputWord.Name = "InputWord";
+            this.InputWord.Size = new System.Drawing.Size(67, 13);
+            this.InputWord.TabIndex = 0;
+            this.InputWord.Text = "Enter a word";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(166, 50);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(290, 50);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(109, 20);
+            this.play.TabIndex = 2;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(193, 103);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.InputWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InputWord;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ wordform/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace wordform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static List<string> LoadWordsList()
+        {
+            List<string> ValidWords = new List<string>();
+
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\words.txt");
+            foreach (var w in File.ReadAllLines(path))
+            {
+                ValidWords.Add(w);
+            }
+
+            return ValidWords;
+        }
+        public static Boolean CheckWord(String input)
+        {
+            List<string> vwords = LoadWordsList();
+            Boolean found = false;
+            foreach (string word in vwords)
+            {
+                if (input.ToLower() == word.ToLower())
+                {
+                    found = true;
+
+                }
+            }
+            return found;
+        }
+
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            string word = InputTextBox.Text;
+            outputLabel.Text = CheckWord(word).ToString();
+        }
+    }
+}
